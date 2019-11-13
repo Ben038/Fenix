9144a9506e539a2c2d39368a7eb328ed4a524280
Benoit Cols <bencols@gmail.com>
2019-11-13 15:14:22 +0100

    upload data ok

--- app/models/accounting_datum.rb
+++ app/models/accounting_datum.rb
@@ -1,6 +1,16 @@
+require 'csv'
+
 class AccountingDatum < ApplicationRecord
   belongs_to :file_upload
   delegate :user, to: :file_upload
   delegate :international_client, to: :user
 
+  def self.import(file)
+    csv_options = { col_sep: ';', headers: true, header_converters: :symbol }
+
+    CSV.foreach(file.path, csv_options) do |row|
+      # row[:balance_year] = row[:balance_year].to_i
+      AccountingDatum.create!(row.to_hash)
+    end
+  end
 end
