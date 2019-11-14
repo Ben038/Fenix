b251d3f39cba7f12760a1cc74c49f78d22847b07
Stephanie Delbecque <stephanie.delbecque@gmail.com>
2019-11-14 12:21:49 +0100

    changes to chart

--- app/javascript/components/chartfunctions.js
+++ app/javascript/components/chartfunctions.js
@@ -10,25 +10,19 @@ var ctx = document.getElementById('myChart');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['2017', '2018', '2019'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Premium',
+            data: [14, 19, 17],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(180, 96, 76, 0.5)',
+                'rgba(41, 52, 98, 0.5)',
+                'rgba(247, 190, 22, 0.5)',
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(180, 96, 76, 1)',
+                'rgba(41, 52, 98, 1)',
+                'rgba(247, 190, 22, 1)',
             ],
             borderWidth: 1
         }]
