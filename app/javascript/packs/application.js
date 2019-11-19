419eac3c2a0b80a8a5fe6af1478cb9c02ecf88fe
Benoit Cols <bencols@gmail.com>
2019-11-19 13:15:31 +0100

    added drap n drop

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,14 @@ import "bootstrap";
 import "../components/chartfunctions";
 import "../components/sidebar";
 import "../components/navbar";
+import Sortable from 'sortablejs';
+
+Sortable.create(sortableKpi, {
+  handle: '.card-kpi',
+  animation: 150
+});
+
+Sortable.create(sortableCharts, {
+  handle: '.card-chart',
+  animation: 150
+});
