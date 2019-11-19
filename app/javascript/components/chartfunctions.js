ef043287110577ef0d18e7ddccc67b9936b6376b
Geraldine Vanderkelen <g.vanderkelen@gmail.com>
2019-11-19 10:47:18 +0100

    polar area charts for comm visualizations done

--- app/javascript/components/chartfunctions.js
+++ app/javascript/components/chartfunctions.js
@@ -1,3 +1,3 @@
 import './chartfunctions/_create_bar_chart'
 import './chartfunctions/_create_line_chart'
-import './chartfunctions/_create_map_chart'
+import './chartfunctions/_create_polararea_chart'
--- app/javascript/components/chartfunctions/_create_polararea_chart.js
+++ app/javascript/components/chartfunctions/_create_polararea_chart.js
@@ -25,13 +25,20 @@
 
 
 var polarArea1= document.getElementById('reinsComm');
+console.log(polarArea1)
+// 1. parse the data-country
+
+//2. parse the data-process
+//3. transform into abs the values in data-process
+
+
 
 var reinsComm = new Chart(polarArea1, {
 type: 'polarArea',
 data : {
-    labels: ['Red', 'Yellow', 'Blue'],
+    labels: JSON.parse(polarArea1.dataset.countries),
     datasets: [{
-        data: [10, 20, 30],
+        data: JSON.parse(polarArea1.dataset.processJson),
         backgroundColor: [
                   'rgba(41, 52, 98, 0.6)',
                   'rgba(41, 52, 98, 0.8)',
@@ -48,9 +55,9 @@ var polarArea2= document.getElementById('brokerComm');
 var brokerComm = new Chart(polarArea2, {
 type: 'polarArea',
 data : {
-    labels: ['Red', 'Yellow', 'Blue'],
+    labels: JSON.parse(polarArea2.dataset.countries),
     datasets: [{
-        data: [10, 20, 30],
+        data: JSON.parse(polarArea2.dataset.processJson),
         backgroundColor: [
                   'rgba(41, 52, 98, 0.6)',
                   'rgba(41, 52, 98, 0.8)',
