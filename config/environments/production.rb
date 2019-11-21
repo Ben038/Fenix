173160cf743759179673d3c90628814c9dffcffd
Benoit Cols <bencols@gmail.com>
2019-11-21 11:58:02 +0100

    heroku fix part 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "www.fenix-analytics.com" }
+  config.action_mailer.default_url_options = { host: "www.fenix-2019.herokuapp.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
