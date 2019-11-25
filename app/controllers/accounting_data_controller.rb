e9a859485682ebb384eedd800d6123fbb2a46b59
Geraldine Vanderkelen <g.vanderkelen@gmail.com>
2019-11-25 14:19:53 +0100

    cleaning code for controllers and helpers

--- app/controllers/accounting_data_controller.rb
+++ app/controllers/accounting_data_controller.rb
@@ -15,16 +15,4 @@ class AccountingDataController < ApplicationController
     AccountingDatum.import(params[:file])
     redirect_to accounting_data_path
   end
-
-  def destroy
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def update
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,18 @@
 class UsersController < ApplicationController
   before_action :find_international_client
 
-# this is a B2B approach and the user buy the product shall not be creating
-# its own access to the platform. A platform admin will create the credentials for each user to
-# make sure the access will be granted only to the dedicated environment
-# for the multinational company
+  # this is a B2B approach and the user buy the product shall not be creating
+  # its own access to the platform. A platform admin will create the credentials for each user to
+  # make sure the access will be granted only to the dedicated environment
+  # for the multinational company
 
   def new
     @user = User.new
   end
 
-  # def create
-  # end
-
   def index
     @users = User.all
   end
-
-  # def destroy
-  # end
-
-  # def show
-  # end
-
-  # def edit
-  # end
-
-  # def update
-  # end
 
   private
 
@@ -35,18 +20,3 @@ class UsersController < ApplicationController
     @international_client = InternationalClient.find(params[:id])
   end
 end
-
-# CODE FROM FINCONSULT NOT NEEDED YET
-
-  # def edit_offered_service
-  #   @user = current_user
-  #   authorize @user
-  # end
-
-  # def remove_offered_service
-  #   @user = current_user
-  #   authorize @user
-  #   offered_service = find_service
-  #   offered_service.destroy
-  #   redirect_to '/user/services'
-  # end
