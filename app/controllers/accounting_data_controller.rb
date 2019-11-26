0e030a68a7c05d44c65117536ab7f03d612c610d
Geraldine Vanderkelen <g.vanderkelen@gmail.com>
2019-11-26 14:29:02 +0100

    identified workaround in the navbar to remove drop-down list and replace it with btn

--- app/controllers/accounting_data_controller.rb
+++ app/controllers/accounting_data_controller.rb
@@ -15,4 +15,16 @@ class AccountingDataController < ApplicationController
     AccountingDatum.import(params[:file])
     redirect_to accounting_data_path
   end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
 end
