91308b6c9d670da4051b83f748c31b0e1c73a864
Geraldine Vanderkelen <g.vanderkelen@gmail.com>
2019-12-04 09:15:55 +0100

    creating a note form to track comments on datasets

--- app/javascript/components/chartfunctions/_create_doghnut_chart.js
+++ app/javascript/components/chartfunctions/_create_doghnut_chart.js
@@ -1,31 +1,22 @@
-var ctx = document.getElementById("myChart");
-if (ctx) {
-var myChart = new Chart(ctx, {
+var pieChart1 = document.getElementById("ClaimPaid");
+if (pieChart1) {
+var ClaimPaid = new Chart(pieChart1, {
   type: 'pie',
   data: {
-    labels: ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'],
     datasets: [{
-      label: '# of Tomatoes',
-      data: [12, 19, 3, 5],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.5)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)'
-      ],
+      data: JSON.parse(pieChart1.dataset.processJson),
+      backgroundColor: [ "#00818a", "#293462", "#f7be16", "#929ed1", "#f9ce4f", "#216583"],
+      borderColor: [ "#00818a", "#293462", "#f7be16", "#929ed1", "#f9ce4f", "#216583"],
       borderWidth: 1
     }]
   },
   options: {
+    maintainAspectRatio: false,
     cutoutPercentage: 40,
     responsive: false,
-
+    legend: {
+      display: false,
+    }
   }
 });
 }
