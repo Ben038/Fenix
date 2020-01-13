7da540c889c166874747264beb43898d979a89c0
Geraldine Vanderkelen <g.vanderkelen@gmail.com>
2020-01-13 15:59:12 +0100

    installing sweetalert-tool-2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,12 @@
+
+//= require sweetalert2
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require jquery.turbolinks
+//= require bootstrap-sprockets
+//= require sweet-alert-confirm
 //= require rails-ujs
 //= require Chart.min
-// = requure 'apexcharts'
+// = require 'apexcharts'
 //= require_tree .
